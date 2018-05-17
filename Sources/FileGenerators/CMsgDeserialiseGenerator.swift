647913442cdad2b1a7b255dccd1277e96e80f180
Carl Lusty <carl.lusty@griffithuni.edu.au>
2018-05-17 13:34:37 +1000

    Fix prototype const

--- Sources/FileGenerators/CMsgDeserialiseGenerator.swift
+++ Sources/FileGenerators/CMsgDeserialiseGenerator.swift
@@ -46,7 +46,7 @@ final public class CMsgDeserialiseGenerator: FileGenerator {
 #include \"guwhiteboardserialisation.h\"
 #include \"guwhiteboard_c_types.h\"
 
-size_t deserialisemsg(WBTypes message_index, void *serialised_in, void *message_out)
+size_t deserialisemsg(WBTypes message_index, const void *serialised_in, void *message_out)
 {
     switch (message_index)
     {

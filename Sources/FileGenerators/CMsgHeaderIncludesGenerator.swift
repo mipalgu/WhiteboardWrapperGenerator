abd1c27584f34fd19fd29f1b8f1b114fcc4e8af6
Carl Lusty <carl.lusty@griffithuni.edu.au>
2019-12-05 16:24:59 +1000

    Remove the gusimplewhiteboard part of the includes here. This was causing a chicken and egg problem when first compiling the whiteboard as the wb required its headers to be installed in usr/local to be compile and then installed in usr/local

--- Sources/FileGenerators/CMsgHeaderIncludesGenerator.swift
+++ Sources/FileGenerators/CMsgHeaderIncludesGenerator.swift
@@ -41,7 +41,7 @@ final public class CMsgHeaderIncludesGenerator: FileGenerator {
 \(customGenClasses.map { entry in 
         return """
 
-#include <gusimplewhiteboard/typeClassDefs/\(NamingFuncs.createCStructName(entry.type)).h>
+#include <typeClassDefs/\(NamingFuncs.createCStructName(entry.type)).h>
 """
         }.reduce("", +)
 )
